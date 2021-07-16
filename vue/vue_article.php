7eb824029a35f8b60a0cbd2e4484c4aaf94befa4
Amandine <amandine.emonet@gmail.com>
2021-07-16 09:50:37 +0200

    Evaluation_PHP_BDD_MVC en 30 minutes

---
+++ vue/vue_article.php
@@ -0,0 +1,23 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ajouter un article</title>
+</head>
+<body>
+<?php
+echo '<h1>Evaluation PHP BDD MVC</h1>';
+echo '<h3>en 30 min à partir d\'une Bdd existente</h3>';
+?>
+    <form action="" method="post">
+        <p>saisir le nom de l'article :</p>
+        <input type="name" name="nom_article">
+        <p>saisir le prix de l'article :</p>
+        <input type="name" name="prix_article">
+        <p>
+            <input type="submit" value="Ajouter">
+        </p>
+    </form>
+</body>
+</html>
